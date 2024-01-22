441784f600895407f585f87f35c4595f2a2f4f6a
Eugene Denkevich <eugenestudio@mail.ru>
2024-01-22 22:47:05 +0300

    add flet, create app

---
+++ frontend/app.py
@@ -0,0 +1,7 @@
+import flet as ft
+
+import settings
+
+
+def main_app(page: ft.Page):
+    page.title = settings.TITLE
---
+++ main.py
@@ -0,0 +1,8 @@
+import flet as ft
+
+from frontend.app import main_app
+
+
+ft.app(
+    target=main_app,
+)
