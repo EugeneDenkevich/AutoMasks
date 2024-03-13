b529eefd9c0efdf48d20733842b7722cb5df537b
Eugene Denkevich <e.denkevich@trainingdata.pro>
2024-03-13 18:46:49 +0300

    fix: creating folder on macos

--- app/backend/src/settings.py
+++ app/backend/src/settings.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
 from pathlib import Path
 import os
+import sys
 
 
 @dataclass
 class Settings:
     API_URL = "https://cvat2.trainingdata.solutions/api"
     # RESULT_PATH = Path(__file__).resolve() / "result"
-    RESULT_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
+    # RESULT_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
+    RESULT_PATH = Path(sys.argv[0])
     TRANSPARENCY: int = 100
 
 
