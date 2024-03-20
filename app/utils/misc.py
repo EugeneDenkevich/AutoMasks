3ae43ea006b279473269fd0bab1d3d3cd5092552
Eugene <eugenestudio@mail.ru>
2024-03-20 13:26:02 +0300

    new

--- app/utils/misc.py
+++ app/utils/misc.py
@@ -29,8 +29,8 @@ def create_result_path() -> str:
     """
     os_name = platform.system()
     if os_name == "Darwin":
-        # result_path = "~/.automask/result/"
-        result_path = Path("~/.automask/result/")
+        result_path = "~/.automask/result/"
+        # result_path = Path("~/.automask/result/")
     else:
         result_path = Path(sys.argv[0]).parent.resolve() / "result"
     if not Path(result_path).exists():
